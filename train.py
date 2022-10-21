d8dbb13cc3c6e9ce082fd385618fd5d8cac14d19
Jin Xiao <ninggujxiao@Gmail.com>
2022-10-21 21:21:43 +0800

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import numpy as np
 import torch
 
-# 准备数据
+
 x=np.load("symm.npy", allow_pickle=True)
 y=np.squeeze(np.load("energy.npy"))
 
@@ -14,11 +14,10 @@ X=np.expand_dims(x,axis=1)
 Y=y.flatten()
 print(X.shape,Y.shape)
 
-# 使用批训练方式
+
 dataset=TensorDataset(torch.tensor(X,dtype=torch.float),torch.tensor(Y,dtype=torch.float))
 dataloader=DataLoader(dataset,batch_size=81,shuffle=True)
 
-# 神经网络主要结构，这里就是一个简单的线性结构
 
 class Net(nn.Module):
     def __init__(self):
@@ -39,13 +38,11 @@ class Net(nn.Module):
 
 net=Net()
 
-# 定义优化器和损失函数
 optim=torch.optim.Adam(Net.parameters(net),lr=0.001)
 Loss=nn.MSELoss()
 
 
-# 下面开始训练：
-# 一共训练 1000次
+
 for epoch in range(1000):
     loss=None
     for batch_x,batch_y in dataloader:
@@ -54,21 +51,6 @@ for epoch in range(1000):
         optim.zero_grad()
         loss.backward()
         optim.step()
-    # 每100次 的时候打印一次日志
     if (epoch+1)%100==0:
         print("step: {0} , loss: {1}".format(epoch+1,loss.item()))
 
-"""
-# 使用训练好的模型进行预测
-predict=net(torch.tensor(X,dtype=torch.float))
-# 绘图展示预测的和真实数据之间的差异
-import matplotlib.pyplot as plt
-plt.plot(x,y,label="fact")
-plt.plot(x,predict.detach().numpy(),label="predict")
-plt.title("sin function")
-plt.xlabel("x")
-plt.ylabel("sin(x)")
-plt.legend()
-#plt.savefig(fname="result.png",figsize=[10,10])
-#plt.show()
-"""
